1f582ba93e88c157a66f64545b0088d51f0a3759
Carlos Moreira <cesmoreira90@hotmail.com>
2022-02-24 19:41:45 -0300

    <feat> Change the code to blocks of if's.

--- checarhorario.php
+++ checarhorario.php
@@ -24,7 +24,7 @@ Segunda - 8h a 12h45 -->
 
 <?php
 
-date_default_timezone_set('America/Sao_Paulo');
+date_default_timezone_set('Etc/GMT+6');
 
 function checarHorario()
 {
@@ -33,32 +33,68 @@ function checarHorario()
     $weekDay = ['Domingo', "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado"];
     $shifts = ["08:00:00", "12:45:00", "18:30:00"];
 
-    for ($i = 0; $i < 8; $i++) {
+    for ($i = 0; $i < 7; $i++) {
         if ($i == $dayNow) {
             $dayNow = $weekDay[$i];
         }
     }
+    echo $dayNow;
+    echo $timeNow;
     if ($dayNow == 'Sabado') {
         if ($timeNow > strtotime($shifts[0]) && $timeNow < strtotime("20:00:00")) {
             echo "Sabado entre 08:00 e 20:00";
         }
-    } else if ($dayNow == 'Domingo') {
+    } 
+    if ($dayNow == 'Domingo') {
         if ($timeNow > strtotime($shifts[0]) && $timeNow < strtotime("16:00:00")) {
             echo "Domingo entre 08:00 e 16:00";
         } else {
             echo "Domingo entre 16:00 e 08:00";
         }
+    } 
+    if ($dayNow == 'Segunda') {
+        if ($timeNow > $shifts[0] && $timeNow < $shifts[1]) {
+            echo $dayNow . " " . "Entre 08:00 e 12:45";
+        } else if ($timeNow > $shifts[1] && $timeNow < $shifts[2]) {
+            echo $dayNow . " " ."entre 12:45 e 18:30";
+        } else {
+            echo $dayNow . " " . "Entre 18:30 e 08:00";
+        }
     }
-    for ($i = 0; $i < 7; $i++) {
-        if ($dayNow == $weekDay[$i]) {
-
-            if ($timeNow  > $shifts[0] && $timeNow < $shifts[1]) {
-                echo $weekDay[$i] . " " . "Entre 08:00 e 12:45";
-            } else if ($timeNow > $shifts[1] && $timeNow < $shifts[2]) {
-                echo $weekDay[$i] . " " . "Entre 12:45 e 18:30";
-            } else {
-                echo $weekDay[$i] . " " . "Entre 18h30 e 08:00";
-            }
+    if ($dayNow == 'Terça') {
+        if ($timeNow > $shifts[0] && $timeNow < $shifts[1]) {
+            echo $dayNow . " " . "Entre 08:00 e 12:45";
+        } else if ($timeNow > $shifts[1] && $timeNow < $shifts[2]) {
+            echo $dayNow . " " ."entre 12:45 e 18:30";
+        } else {
+            echo $dayNow . " " . "Entre 18:30 e 08:00";
+        }
+    }
+    if ($dayNow == 'Quarta') {
+        if ($timeNow > $shifts[0] && $timeNow < $shifts[1]) {
+            echo $dayNow . " " . "Entre 08:00 e 12:45";
+        } else if ($timeNow > $shifts[1] && $timeNow < $shifts[2]) {
+            echo $dayNow . " " ."entre 12:45 e 18:30";
+        } else {
+            echo $dayNow . " " . "Entre 18:30 e 08:00";
+        }
+    }
+    if ($dayNow == 'Quinta') {
+        if ($timeNow > $shifts[0] && $timeNow < $shifts[1]) {
+            echo $dayNow . " " . "Entre 08:00 e 12:45";
+        } else if ($timeNow > $shifts[1] && $timeNow < $shifts[2]) {
+            echo $dayNow . " " ."entre 12:45 e 18:30";
+        } else {
+            echo $dayNow . " " . "Entre 18:30 e 08:00";
+        }
+    }
+    if ($dayNow == 'Sexta') {
+        if ($timeNow > $shifts[0] && $timeNow < $shifts[1]) {
+            echo $dayNow . " " . "Entre 08:00 e 12:45";
+        } else if ($timeNow > $shifts[1] && $timeNow < $shifts[2]) {
+            echo $dayNow . " " ."entre 12:45 e 18:30";
+        } else {
+            echo $dayNow . " " . "Entre 18:30 e 08:00";
         }
     }
 }
